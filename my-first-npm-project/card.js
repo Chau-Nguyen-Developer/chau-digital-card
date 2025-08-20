df968260aedaa4afaea952f88b5cdf2d3196164f
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2025-08-20 02:39:20 +0000

    chore: printed out the word Hello in font 3D ASCII. Still need to work on position and font.

--- my-first-npm-project/card.js
+++ my-first-npm-project/card.js
@@ -3,13 +3,38 @@
 import chalk from "chalk";
 import boxen from "boxen";
 import figlet from "figlet";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from 'url';
+
+//Get the current folder
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+//Path to your font
+const fontPath = path.join(__dirname, 'larry3d');
 
 //Make a big ASCII name
 //const name = figlet.textSync("Hello");
 
 //Style my details with chalk
-const card = boxen(`
 //${figlet.textSync("Hello", {font: "larry3d"})}
+
+
+//const fontPath = "/workspaces/chau-digital-card/fonts/larry3d";
+
+
+const card = boxen(`
+${figlet.text("Hello", {font:"3D-ASCII", horizontalLayout:'default', verticalLayout:'default'}, function(err,data)
+{
+    if(err)
+    {
+        //console.error("Error loading font: ", err);
+        throw err;
+        //return;
+    }
+    console.log(data);
+})}
 ${chalk.bold('Chau Nguyen Developer ')} |  Software Engineer
 ${chalk.bold('-'.repeat(60))}
 
@@ -20,7 +45,7 @@ ${chalk.bold('LinkedIn:')} https://www.linkedin.com/in/ngoc-chau-nguyen-cpp/
 ${chalk.bold('Substack Blog:')} https://ngocchaun1.substack.com/
 
 
-${chalk.bold('Card: ')} npm chauDeveloper
+${chalk.bold('Card: ')} npx chauDeveloper
 `,
     {
         borderColor: 'cyan',
